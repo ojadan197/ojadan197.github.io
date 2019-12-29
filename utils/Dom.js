ac8c4eac32b7c069bb3cadb04e2bc2bdaba779b0
Omar Jadan <o.jadan197@gmail.com>
2019-12-28 22:23:29 -0500

    Changed Master for Github version

--- utils/Dom.js
+++ utils/Dom.js
@@ -1,64 +1,64 @@
-// Creating Menu Tab Elements Here
+// // Creating Menu Tab Elements Here
 
-const tab = document.createElement("div");
-tab.classList.add("tab-container__colorized");
-// tab.append("Hello World")
+// const tab = document.createElement("div");
+// tab.classList.add("tab-container__colorized");
+// // tab.append("Hello World")
 
-const tab_1  =  document.createElement("div");
-tab_1.classList.add("tab-container__colorized");
-tab_1.append("About \n Me");
-tab_1.id = 'tab_1';
+// const tab_1  =  document.createElement("div");
+// tab_1.classList.add("tab-container__colorized");
+// tab_1.append("About \n Me");
+// tab_1.id = 'tab_1';
 
-const tab_2  = document.createElement("div");
-tab_2.classList.add("tab-container__colorized");
-tab_2.append("Future Goals");
-tab_2.id = 'tab_2';
+// const tab_2  = document.createElement("div");
+// tab_2.classList.add("tab-container__colorized");
+// tab_2.append("Future Goals");
+// tab_2.id = 'tab_2';
 
-const tab_3  = document.createElement("div");
-tab_3.classList.add("tab-container__colorized");
-tab_3.append("Past Experience");
-tab_3.id = 'tab_3';
+// const tab_3  = document.createElement("div");
+// tab_3.classList.add("tab-container__colorized");
+// tab_3.append("Past Experience");
+// tab_3.id = 'tab_3';
 
-const tab_4  = document.createElement("div");
-tab_4.classList.add("tab-container__colorized");
-tab_4.append("Katas and Treasures");
-tab_4.id = 'tab_4';
+// const tab_4  = document.createElement("div");
+// tab_4.classList.add("tab-container__colorized");
+// tab_4.append("Katas and Treasures");
+// tab_4.id = 'tab_4';
 
-const tab_5  = document.createElement("div");
-tab_5.classList.add("tab-container__colorized");
-tab_5.append("Sample Projects");
-tab_5.id = 'tab_5';
+// const tab_5  = document.createElement("div");
+// tab_5.classList.add("tab-container__colorized");
+// tab_5.append("Sample Projects");
+// tab_5.id = 'tab_5';
 
-const tab_6 = document.createElement("div");
-tab_6.classList.add("tab-container__colorized__result");
-tab_6.id = 'tab_6';
-
-
-
-
-// Tab Manipulation
-// Populating the Main Menu of Tabs
-function populate_tabs() {
-    const menu_tabs = document.querySelector("#menu_tabs");
-    menu_tabs.append(tab_1);
-    menu_tabs.append(tab_2);
-    menu_tabs.append(tab_3);
-    menu_tabs.append(tab_4);
-    menu_tabs.append(tab_5);
-  }
+// const tab_6 = document.createElement("div");
+// tab_6.classList.add("tab-container__colorized__result");
+// tab_6.id = 'tab_6';
 
 
 
 
+// // Tab Manipulation
+// // Populating the Main Menu of Tabs
+// function populate_tabs() {
+//     const menu_tabs = document.querySelector("#menu_tabs");
+//     menu_tabs.append(tab_1);
+//     menu_tabs.append(tab_2);
+//     menu_tabs.append(tab_3);
+//     menu_tabs.append(tab_4);
+//     menu_tabs.append(tab_5);
+//   }
 
-  //Tab Content String Variables
-  //Contents of Tab 6 after Tab 1 has been Clicked
-  const content_1 = `This is the about me section,
-   currently under construction`;
+
+
+
+
+//   //Tab Content String Variables
+//   //Contents of Tab 6 after Tab 1 has been Clicked
+//   const content_1 = `This is the about me section,
+//    currently under construction`;
   
-  const content_2 = `This is the content of Block 2 -- Future Goals`;
-  const content_3 = `This is the content of Block 3 -- Past Experience`;
-  const content_4 = `This is the content of Block 4 -- Katas and Treasures`;
-  const content_5 = `This is the content of Block 5 -- Sample Projects`;
+//   const content_2 = `This is the content of Block 2 -- Future Goals`;
+//   const content_3 = `This is the content of Block 3 -- Past Experience`;
+//   const content_4 = `This is the content of Block 4 -- Katas and Treasures`;
+//   const content_5 = `This is the content of Block 5 -- Sample Projects`;
 
 
